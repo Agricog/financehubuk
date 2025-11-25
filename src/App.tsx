115790592b2b17936bfbde0ecdfe6c596aeceac5
Agricog <mick@mickstevenson.com>
2025-11-25 21:29:53 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import HomePage from './pages/HomePage'
 import MortgageCalculatorPage from './pages/MortgageCalculatorPage'
-import LoanCalculatorPage from './pages/LoanCalculatorPage'
+import PersonalLoanCalculatorPage from './pages/PersonalLoanCalculatorPage'
 import HomeInsuranceQuotePage from './pages/HomeInsuranceQuotePage'
 import CarInsuranceQuotePage from './pages/CarInsuranceQuotePage'
 import LifeInsuranceQuotePage from './pages/LifeInsuranceQuotePage'
 import PetInsuranceQuotePage from './pages/PetInsuranceQuotePage'
-import Navigation from './components/Navigation'
-import Footer from './components/Footer'
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage'
+import TermsOfServicePage from './pages/TermsOfServicePage'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navigation />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/calculators/mortgage-calculator" element={<MortgageCalculatorPage />} />
-            <Route path="/calculators/loan-calculator" element={<LoanCalculatorPage />} />
-            <Route path="/calculators/home-insurance-quote" element={<HomeInsuranceQuotePage />} />
-            <Route path="/calculators/car-insurance-quote" element={<CarInsuranceQuotePage />} />
-            <Route path="/calculators/life-insurance-quote" element={<LifeInsuranceQuotePage />} />
-            <Route path="/calculators/pet-insurance-quote" element={<PetInsuranceQuotePage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/calculators/mortgage-calculator" element={<MortgageCalculatorPage />} />
+          <Route path="/calculators/personal-loan-calculator" element={<PersonalLoanCalculatorPage />} />
+          <Route path="/calculators/home-insurance-quote" element={<HomeInsuranceQuotePage />} />
+          <Route path="/calculators/car-insurance-quote" element={<CarInsuranceQuotePage />} />
+          <Route path="/calculators/life-insurance-quote" element={<LifeInsuranceQuotePage />} />
+          <Route path="/calculators/pet-insurance-quote" element={<PetInsuranceQuotePage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms-of-service" element={<TermsOfServicePage />} />
+        </Routes>
+      </Layout>
     </Router>
   )
 }
 
 export default App
+
 
