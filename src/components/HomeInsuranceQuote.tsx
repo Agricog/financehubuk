700d869ccd75d5122ddc1edadb800011eb039916
Agricog <mick@mickstevenson.com>
2025-11-26 15:08:32 +0000

    Update HomeInsuranceQuote.tsx

--- src/components/HomeInsuranceQuote.tsx
+++ src/components/HomeInsuranceQuote.tsx
@@ -193,7 +193,7 @@ export default function HomeInsuranceQuotePage() {
             <iframe
               src="https://app.smartsuite.com/form/sba974gi/l5qQJVsntQ?header=false&Prefill_Registration+Source=Home-Insurance"
               width="100%"
-              height="600"
+              height="350"
               frameBorder="0"
               title="Home Insurance Quote Form"
               className="rounded-lg border border-gray-200"
