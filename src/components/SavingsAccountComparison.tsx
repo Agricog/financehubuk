e951522c32793ca12efd54e873e4d3040224668c
Agricog <mick@mickstevenson.com>
2025-11-26 18:41:04 +0000

    Update SavingsAccountComparison.tsx

--- src/components/SavingsAccountComparison.tsx
+++ src/components/SavingsAccountComparison.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { ArrowLeft, TrendingUp } from 'lucide-react'
+import { ArrowLeft } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 interface SavingsAccount {
@@ -263,3 +263,4 @@ export default function SavingsAccountComparison() {
     </div>
   )
 }
+
